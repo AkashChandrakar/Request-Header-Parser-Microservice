24644d2ec7dd3cce532b688a34e046f66922affb
Akash Chandrakar <akashchandrakar0@gmail.com>
2016-09-13 17:58:19 +0000

    heroku support

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var app = express();
 
 app.get('/',function(req,res){
   res.setHeader('Content-Type', 'application/json');
-  res.send({ipaddress:req.headers['x-forwarded-for'],language:req.acceptedLanguages[0],operating_system:req.headers['user-agent']});
+  var ipAddress=req.headers['x-forwarded-for']||null;
+  var lang = req.acceptedLanguages[0]||null;
+  var user = req.headers['user-agent'];
+  var os = user.substring(user.search("\\(")+1,user.search("\\)"))||null;
+  res.send({ipaddress:ipAddress,language:lang,operating_system:os});
 });
 app.listen(process.env.PORT||8080, function () {
   console.log('Example app listening on port 8080!');
